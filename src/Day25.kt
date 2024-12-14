20186442b22acdc5d7b92083676e795ddb362d70
Zarah Dominguez <zarah@zarah.dev>
2024-12-14 21:49:37 +1100

    Utils and templates

---
+++ src/Day25.kt
@@ -0,0 +1,19 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // Test if implementation meets criteria from the description:
+    val testInput = readInput("Day01_test")
+    check(part1(testInput) == 1)
+    check(part2(testInput) == 1)
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("Day01")
+    println("**** Part 1: Sum = ${part1(input)}")
+    println("**** Part 2: Sum = ${part2(input)}")
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -6,7 +6,7 @@ import kotlin.io.path.readText
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
+fun readInput(name: String) = Path("src/inputs/$name.txt").readText().trim().lines()
 
 /**
  * Converts string to md5 hash.
